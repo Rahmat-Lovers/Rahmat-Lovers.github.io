990f791dadcee4da309d5cd21a62149e8788aaaf
salismazaya <salismazaya@gmail.com>
2023-03-18 05:11:54 +0700

    mengganti router

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,32 @@
 <script>
-	import Router from 'svelte-spa-router';
-	import routes from './routes';
+	import { Router, Link, Route } from "svelte-routing";
+	import Login from "./routes/Login.svelte";
+	import User from "./routes/User.svelte";
+	import UserSettings from "./routes/UserSettings.svelte";
 
-	const canUsing = !!(window.ethereum)
+	const canUsing = !!window.ethereum;
+
+	export let url = "";
 </script>
 
-
 {#if canUsing}
-<Router {routes} />
+	<Router url="{url}">
+		<Route path="/home/user/settings" component={UserSettings} />
+		<Route path="/home/user" component={User} />
+		<Route path="/" component={Login} />
+	</Router>
 {:else}
-<div class="h-screen w-full fixed z-auto flex items-center bg-gray-200 px-5">
-	<div class="w-full text-center text-4xl">
-		<p>Tidak bisa menggunakan situs ini. Pastikan ektensi metamask sudah terinstall. Jika menggunakan mobile mohon gunakan browser brave</p>
+	<div
+		class="h-screen w-full fixed z-auto flex items-center bg-gray-200 px-5"
+	>
+		<div class="w-full text-center text-4xl">
+			<p>
+				Tidak bisa menggunakan situs ini. Pastikan ektensi metamask
+				sudah terinstall. Jika menggunakan mobile mohon gunakan browser
+				brave
+			</p>
+		</div>
 	</div>
-</div>
 {/if}
 
 <style>
--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,17 +1,18 @@
 <script>
     import provider from "./../data/provider";
-    import { replace } from "svelte-spa-router"
+    import { navigate } from "svelte-routing";
+    import Swal from "sweetalert2";
 
-    // let loginAs;
+    let loginAs;
 
     const login = async () => {
         
-        // if (!loginAs) {
-        //     alert("anda Belum memilih salah satu Opsi");
-        //     return;
-        // }
+        if (!loginAs) {
+            await Swal.fire("anda Belum memilih salah satu Opsi");
+            return;
+        }
         await provider.send("eth_requestAccounts", []);
-        await replace('/home/user')
+        await navigate('/home/' + loginAs)
     };
 
 </script>
@@ -23,7 +24,9 @@
         <div class=" flex justify-center items-center mt-1 mb-2 p-2 w-full h-auto">
             <ul>
                 <li class=" relative block list-none mb-2">
-                    <button class="w-full h-10 py-2 px-28 bg-white rounded-xl outline-none active:bg-blue-300 focus:ring-2 ring-blue-500" onclick="alert('anda pilih acceptor')" id="user">
+                    <button class="w-full h-10 py-2 px-28 bg-white rounded-xl outline-none active:bg-blue-300 focus:ring-2 ring-blue-500" id="user" on:click={() => {
+                        loginAs = 'acceptor';
+                    }}>
                         <span class="absolute top-1 left-1 w-8 h-8 rounded-full bg-slate-100">
                             <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -33,7 +36,9 @@
                     </button>
                 </li>
                 <li class=" relative block list-none mt-5">
-                    <button class="w-full h-10 bg-white rounded-xl outline-none active:bg-blue-300 focus:ring-2 ring-blue-500" onclick="alert('anda pilih User')">
+                    <button class="w-full h-10 bg-white rounded-xl outline-none active:bg-blue-300 focus:ring-2 ring-blue-500" on:click={() => {
+                        loginAs = 'user';
+                    }}>
                         <span class="absolute top-1 left-1 w-8 h-8 rounded-full bg-slate-100">
                             <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" stroke-linecap="round" stroke-linejoin="round"></path>
